9c00099a11e5ebe3c49c39d662ca20d07a1336d5
Karun Agarwal <karun.agarwal@esmagico.in>
2023-05-31 12:06:25 +0530

    Sidebars update

---
+++ docs/devops/add-vms-to-swarm.md
@@ -0,0 +1,8 @@
+---
+title: Adding newly added VMs to Swarm
+---
+
+<head>
+  <title>Adding newly added VMs to Swarm</title>
+  <meta name="description" content="Overview of Infra management for Amakrush" />
+</head>
--- sidebars.js
+++ sidebars.js
@@ -16,7 +16,13 @@ module.exports = {
       type: 'category',
       label: 'Infra',
       collapsed: false,
-      items: ['infra/overview', 'infra/setting-up', 'infra/scaling-disk-size'],
+      items: ['infra/overview', 'infra/setting-up', 'infra/scaling-disk-size', 'infra/scaling-vm-horizontally'],
+    },
+    {
+      type: 'category',
+      label: 'Devops',
+      collapsed: false,
+      items: ['devops/add-vms-to-swarm'],
     },
   ],
 
